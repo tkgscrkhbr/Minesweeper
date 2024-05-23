aa92a3e15a782c0b08753e63a59345776a17aaae
Elad Berliner <143136428+tkgscrkhbr@users.noreply.github.com>
2024-05-23 14:29:48 +0300

    Beta 1.1
    fixed many bug, added an hints func
    muy divertido

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 //                     (@@@@@@@  @@@%         @@@@%      @@@@@@@@                                      
 //                     (@@@      @@@%        @@@@@@/     @@@   @@@@*                                   
 //                     (@@@@@@@  @@@%       @@@@ @@@.    @@@    .@@@                                   
@@ -16,7 +15,7 @@
 //    .@@@@@@@@@.  @@@@@@@. %@@@  @@@@   @@@@@@@/ @@@@   @@@     @@@@@   @@@@@@@  @@@@ .@@@@           
 
 
-
+var isHint = false
 var recursionboolean = true
 var firstClick = true
 var gGamestate = false
@@ -24,9 +23,11 @@ var gNotMines = 0
 var gCurrNotMines = 0
 var gMines = 0
 var gBoard
-var gShown=0
+var gShown = 0
 var gLife = 3
-var gMarked=0
+var gMarked = 0
+var hintsLeft=3
+var bulbState=true
 const numMines = 2 // Set the desired number of mines here
 onInit()
 
@@ -38,10 +39,6 @@ function onInit() {
 
 
 }
-
-
-
-
 
 function buildBoard(size) {
   if (gLife === 0) return
@@ -80,7 +77,7 @@ function calcAllMines() {
 }
 
 function calcAllNotMines() {
- gNotMines=0
+  gNotMines = 0
   for (var i = 0; i < gBoard.length; i++) {
     for (var j = 0; j < gBoard[0].length; j++) {
       if (gBoard[i][j].isMine === false) {
@@ -90,30 +87,27 @@ function calcAllNotMines() {
   }
 }
 
-function calcAllShown(){
-  gShown=0
+function calcAllShown() {
+  gShown = 0
   for (var i = 0; i < gBoard.length; i++) {
     for (var j = 0; j < gBoard[0].length; j++) {
-      if (gBoard[i][j].isShown&&!gBoard[i][j].isMine) {
+      if (gBoard[i][j].isShown && !gBoard[i][j].isMine) {
         gShown++
       }
     }
   }
 }
 
-function calcAllMarked(){
-  gMarked=0
+function calcAllMarked() {
+  gMarked = 0
   for (var i = 0; i < gBoard.length; i++) {
     for (var j = 0; j < gBoard[0].length; j++) {
-      if (gBoard[i][j].isMarked === true||gBoard[i][j].isMine&&gBoard[i][j].isShown) {
+      if (gBoard[i][j].isMarked === true || gBoard[i][j].isMine && gBoard[i][j].isShown) {
         gMarked++
       }
     }
   }
 }
-
-
-
 
 function setMinesNegsCount(board, cellI, cellJ) {
   var MineCounter = 0
@@ -130,8 +124,6 @@ function setMinesNegsCount(board, cellI, cellJ) {
 }
 
 
-
-
 function renderBoard(board) {
   if (gLife === 0) return
   var strHTML = ''
@@ -143,21 +135,22 @@ function renderBoard(board) {
       const isShown = board[i][j].isShown
       const className = `cell cell-${i}-${j} isShown-${isShown}`
 
-      strHTML += `<td class="${className}" onclick="onCellClicked(this,${i},${j})"oncontextmenu="onCellMarked(this,${i},${j})">${!cell.isMarked ? (cell.isShown ? (cell.isMine ? '💣' : cell.minesAroundCount!==0?cell.minesAroundCount:"") : '') : '🏴'}</td>`
+      strHTML += `<td class="${className}" onclick="onCellClicked(this,${i},${j})"oncontextmenu="onCellMarked(this,${i},${j})">${!cell.isMarked ? (cell.isShown ? (cell.isMine ? '💣' : cell.minesAroundCount !== 0 ? cell.minesAroundCount : "") : '') : '🏴'}</td>`
     }
     strHTML += '</tr>'
   }
   const elBoard = document.querySelector('.board')
   elBoard.innerHTML = strHTML
-if(gLife>0){
-  document.querySelector('h2').innerText = `${gLife} LIVES LEFT`
-  calcAllNotMines()
-  calcAllShown()
-  calcAllMarked()
+  if (gLife > 0) {
+    document.querySelector('h2').innerText = `${gLife} LIVES LEFT`
+    calcAllNotMines()
+    calcAllShown()
+    calcAllMarked()
 
-}
-else{ document.querySelector('h2').innerText = 'You Lost the Game'
-}
+  }
+  else {
+    document.querySelector('h2').innerText = 'You Lost the Game'
+  }
 }
 //prevents right click
 document.addEventListener('contextmenu', function (event) {
@@ -178,17 +171,17 @@ function onCellMarked(board, i, j) {
 
 function recursionFunc(cell, i, j) {
   for (var x = i - 1; x <= i + 1; x++) {
-    if (x < 0 || x >= gBoard.length) continue;
+    if (x < 0 || x >= gBoard.length) continue
     for (var y = j - 1; y <= j + 1; y++) {
-      if (y < 0 || y >= gBoard[0].length) continue;
-      if (gBoard[x][y].isShown) continue;
-      gBoard[x][y].isShown = true;
+      if (y < 0 || y >= gBoard[0].length) continue
+      if (gBoard[x][y].isShown) continue
+      gBoard[x][y].isShown = true
 
       if (gBoard[x][y].minesAroundCount === 0) {
-        recursionboolean=true
+        recursionboolean = true
         // console.log(cell,x,y)
-        firstClickFunc(cell, x, y);
-        
+        firstClickFunc(cell, x, y)
+
       }
     }
   }
@@ -198,6 +191,7 @@ function recursionFunc(cell, i, j) {
 
 
 function firstClickFunc(cell, i, j) {
+
   if (firstClick) {
     mineMaker(gBoard.length, numMines, i, j)
     renderBoard(gBoard)
@@ -210,14 +204,14 @@ function firstClickFunc(cell, i, j) {
     recursionboolean = false
     console.log(gBoard[i][j].minesAroundCount)
   }
-    if (gBoard[i][j].minesAroundCount === 0) {
-      recursionFunc(cell, i, j)
+  if (gBoard[i][j].minesAroundCount === 0) {
+    recursionFunc(cell, i, j)
 
-    }
-    recursionboolean = false
+  }
+  recursionboolean = false
 
 
-  
+
 
 
 }
@@ -227,6 +221,14 @@ function firstClickFunc(cell, i, j) {
 
 
 function onCellClicked(cell, i, j) {
+  if (isHint === true&&!firstClick) {
+    showHint(cell, i, j)
+    console.log('yes')
+    return}
+    if(firstClick&&isHint===true){
+      alert('hints are not allowed before 1st pick')
+      return
+    }
   if (gBoard[i][j].isMarked) return
   if (firstClick) {
     gBoard[i][j].isShown = true
@@ -258,29 +260,29 @@ function onCellClicked(cell, i, j) {
 
 function mineMaker(size, numMines, firstClickI, firstClickJ) {
   // Reset the mine count
-  gMines = 0;
+  gMines = 0
 
   // Place the mines
   while (gMines < numMines) {
-    const i = Math.floor(Math.random() * size);
-    const j = Math.floor(Math.random() * size);
+    const i = Math.floor(Math.random() * size)
+    const j = Math.floor(Math.random() * size)
 
     // Avoid placing a mine at the first click position
-    if ((i === firstClickI && j === firstClickJ) || gBoard[i][j].isMine) continue;
+    if ((i === firstClickI && j === firstClickJ) || gBoard[i][j].isMine) continue
 
-    gBoard[i][j].isMine = true;
-    gMines++;
+    gBoard[i][j].isMine = true
+    gMines++
   }
 
   // Calculate mines around each cell after placing all mines
-  calcAllMines();
+  calcAllMines()
 }
 
 
 function showMines() {
   for (var i = 0; i < gBoard.length; i++) {
     for (var j = 0; j < gBoard.length; j++) {
-      if (gBoard[i][j].isMine === true){ gBoard[i][j].isShown = true}
+      if (gBoard[i][j].isMine === true) { gBoard[i][j].isShown = true }
 
 
     }
@@ -306,11 +308,14 @@ function startNewGame() {
   gNotMines = 0
   firstClick = true
   recursionFunc = true
+  bulbState=false
+  isHint=true
+  bulbIsShow()
 }
 
 
 function checkWin() {
-  if (gShown===gNotMines&&gGamestate===true&&gMarked===numMines) {
+  if (gShown === gNotMines && gGamestate === true && gMarked === numMines) {
     winGame()
   }
 
@@ -321,3 +326,65 @@ function winGame() {
   document.querySelector('.smiley').innerText = '😎'
   gGamestate = false
 }
+
+
+
+
+function hintFunc() {
+  const currHint = document.querySelector('.hint')
+
+  if (!isHint) {
+    currHint.innerHTML = '<img src="svg/hinton.svg">'
+    isHint = true
+  }
+  else {
+    currHint.innerHTML = '<img src="svg/hintOff2.svg">'
+    isHint = false
+  }
+
+
+}
+function bulbIsShow(){
+  var TempBulbState =document.querySelector('.hint')
+  var tempHintState= document.querySelector('h5')
+
+  if(bulbState===true&&hintsLeft===0){
+  tempHintState.style.display='none'
+  TempBulbState.style.display='none'
+}
+else{
+  tempHintState.style.display=''
+  TempBulbState.style.display=''
+  bulbState=true
+  hintsLeft=3
+  document.querySelector('h5').innerHTML=`${hintsLeft} Hints are left`
+  hintFunc()
+}
+}
+
+
+
+
+
+function showHint(cell, i, j) {
+  hintsLeft--
+  if(hintsLeft===0)bulbIsShow()
+  else hintFunc()
+document.querySelector('h5').innerHTML=`${hintsLeft} hints are left`
+  for (var x = i - 1; x <= i + 1; x++) {
+    if (x < 0 || x >= gBoard.length) continue
+    for (var y = j - 1; y <= j + 1; y++) {
+      if (y < 0 || y >= gBoard[0].length) continue
+      if (gBoard[x][y].isShown) continue
+      gBoard[x][y].isShown = true
+      renderBoard(gBoard)
+      setTimeout(hintTime, 1500,x, y)
+
+    }
+  }
+}
+function hintTime(i, j)  {
+  gBoard[i][j].isShown=false
+  
+  renderBoard(gBoard)
+    }
